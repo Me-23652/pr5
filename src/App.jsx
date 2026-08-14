3cb8ae3ac6c5e52740f8ca2cd9657098431cc7a9
vatsal123173 <pv803152@gmail.com>
2026-08-14 13:34:56 +0530

    sat

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"; 
+import PasswordInput from "./Component/PasswordInput";
+function App() {   
+  return (     
+  <div>       
+    <PasswordInput />     
+    </div>   
+    ); 
+  }  
+  export default App; 
